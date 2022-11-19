38f55a2d4d893df2073630ad968f59339ffb85c0
Andres Wilchinski <andreswil97@gmail.com>
2022-11-19 17:17:06 -0300

    fet: create 2 trigger

--- triggers.sql
+++ triggers.sql
@@ -13,7 +13,7 @@
 
 SET serveroutput ON;
 
-
+-- 1. Todo usuario registrado debe ser mayor a 18 años
 CREATE OR REPLACE TRIGGER VALIDAR_EDAD_USUARIO
 BEFORE INSERT OR UPDATE ON USUARIO
 FOR EACH ROW
@@ -23,7 +23,7 @@ BEGIN
     VEdad := MONTHS_BETWEEN(SYSDATE, :NEW.fechaNacimiento)/12; 
     IF VEdad < 18  THEN
         DBMS_OUTPUT.PUT_LINE ('El usuario debe ser mayor de edad');
-        RAISE_APPLICATION_ERROR(-20001,'El usuario debe ser mayor de edad ' ||TO_CHAR(VEdad)|| ' años') ;
+        RAISE_APPLICATION_ERROR(-20001,'El usuario debe ser mayor de edad, actualmente posee ' ||TO_CHAR(VEdad)|| ' años') ;
     END IF;
 END;
 
@@ -34,3 +34,26 @@ INSERT INTO Usuario (email, nombre, apellido, fechaNacimiento, idMercado, fechaC
 UPDATE Usuario
 SET fechaNacimiento = TO_DATE('24/01/2008', 'dd/mm/yyyy')
 WHERE email='nicolasandreoli@gmail.com';
+
+-- 2. La factura debe tener el mismo codigoMedioPago que el de usuario que se relaciona con la factura
+
+CREATE OR REPLACE TRIGGER VALIDAR_MEDIO_PAGO_ES_EL_MISMO
+BEFORE INSERT OR UPDATE ON FACTURA
+FOR EACH ROW
+DECLARE
+    VUsuarioMedioPago MedioPago.codigo%TYPE;
+BEGIN
+    SELECT codigo INTO VUsuarioMedioPago  
+    FROM MedioPago M
+    WHERE :NEW.emailUsuario = M.emailUsuario;
+    IF :NEW.codigoMedioPago != VUsuarioMedioPago  THEN
+        RAISE_APPLICATION_ERROR(-20001, 'La factura debe tener el mismo medio de pago que el usuario que paga la factura') ;
+    END IF;
+END;
+
+-- test
+INSERT INTO Factura (id, total, fecha, emailusuario, codigoMedioPago) VALUES (7, 700, TO_DATE('17/10/2021', 'dd/mm/yyyy'), 'josemaria@gmail.com', '123456');
+
+UPDATE Factura
+SET codigoMedioPago = '123456'
+WHERE id=1;
