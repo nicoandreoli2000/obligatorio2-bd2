aa54b656c5643f2449523103f7e2935d23ac0e45
Nicolas Andreoli <nicolasandreoli2000@hotmail.com>
2022-11-22 19:15:02 -0300

    feat: could not test it because oracledb is not available for mac

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
-const express = require("express");
-const app = express();
+const mongodb = require("mongodb");
+const oracledb = require("oracledb");
 
-app.get("/", (req, res) => {
-  res.send("I will be shown on the Browser");
-  console.log("I will be shown on the Terminal");
-});
+const run = async () => {
+  const data = await getDataFromOracle();
+  await insertDataIntoMongo(data);
+};
 
-app.listen(3000);
+const getDataFromOracle = async () => {
+  let connection;
+
+  try {
+    connection = await oracledb.getConnection({
+      user: "",
+      password: "",
+      connectString: "",
+    });
+
+    const result = await connection.execute(
+      `SELECT v.numeroDeSerie, a.modelo
+       FROM Producto p, Venta v, Automovil a
+       WHERE p.id = a.id
+       AND f.idProducto = p.id;`
+    );
+
+    return result.rows;
+  } catch (err) {
+    console.error(err);
+  } finally {
+    if (connection) {
+      try {
+        await connection.close();
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
+};
+
+const insertDataIntoMongo = async (data) => {
+  //TODO
+  console.log(data);
+};
+
+run();
